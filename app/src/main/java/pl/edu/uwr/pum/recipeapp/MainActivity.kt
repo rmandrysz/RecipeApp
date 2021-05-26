16de2130b31f5db6493b1650e0a9e0aaf9ca2a15
Rafał Mandrysz <rafal.mandrysz@gmail.com>
2021-05-26 17:55:41 +0200

    Implement navigation between the list fragment and the dialog

--- app/src/main/java/pl/edu/uwr/pum/recipeapp/MainActivity.kt
+++ app/src/main/java/pl/edu/uwr/pum/recipeapp/MainActivity.kt
@@ -3,6 +3,11 @@ package pl.edu.uwr.pum.recipeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -13,9 +18,20 @@ import pl.edu.uwr.pum.recipeapp.model.entities.Recipe
 import pl.edu.uwr.pum.recipeapp.model.relations.RecipeIngredientCrossRef
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val dao: RecipeDAO = RecipeDatabase.getInstance(this).recipeDao()
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
