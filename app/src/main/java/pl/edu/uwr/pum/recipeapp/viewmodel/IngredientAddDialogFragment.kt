f8517b93111855f43cc5a5580cdd54329a8c6976
Rafał Mandrysz <rafal.mandrysz@gmail.com>
2021-05-31 18:07:43 +0200

    Update the layout of the ingredient adding dialog

--- app/src/main/java/pl/edu/uwr/pum/recipeapp/viewmodel/IngredientAddDialogFragment.kt
+++ app/src/main/java/pl/edu/uwr/pum/recipeapp/viewmodel/IngredientAddDialogFragment.kt
@@ -35,7 +35,7 @@ class IngredientAddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ingredient_add, container, false)
+        return inflater.inflate(R.layout.fragment_ingredient_add_dialog, container, false)
     }
 
     companion object {
